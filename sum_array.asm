ee94921031497b783c307b61ce6b33c72f501256
marcosraimondi <marcosraimondi1@hotmail.com>
2024-04-22 15:00:27 -0300

    saque linea que no hacia falta

--- sum_array.asm
+++ sum_array.asm
@@ -31,8 +31,6 @@ for_loop:
         cmp edx, [ebp+12]       ; comparar i con size
         jge end_loop            ; jump if (edx) greater or equal
         
-        add dword [eax+edx*4], edx ; input[i] += 1
-
         fld dword [eax+edx*4]   ; guardar float en la pila de la FPU
         fistp dword [ecx+edx*4] ; pasamos a entero y guardamos en output[i]
         inc dword [ecx+edx*4]   ; output[i] += 1
